3dd686bfccd18044a30df88e49c7bde3ec0693f7
youlei <zha_zi@126.com>
2018-03-06 14:12:09 +0800

    提交挨骂

--- index.js
+++ index.js
@@ -4,76 +4,80 @@
  * @auth lei.you
  * @date 2018/3/5
  * */
-console.log('supreme-cli start....................');
+
 var exec = require('child_process').exec;
-var program = require('commander');
+//var program = require('commander');
 var copydir = require('copy-dir');
 var fs = require('fs');
 var path = require('path');
+const chalk = require('chalk');
+console.log(chalk.blue('supreme-cli start...'));
 //当前执行程序的路径（执行命令行时候的路径,不是代码路径 例如 在根目录下执行 node ./xxx/xxx/a.js 则 cwd 返回的是 根目录地址 ）
 //console.log(process.execPath)
 //__dirname: 代码存放的位置
 //console.log(__dirname)
 //当前执行的node路径（如：/bin/node）
 ///console.log(process.cwd())
-exec("git clone git@git.nevint.com:arthur/supreme.git",function(error){
-    if(!error){
-        var supremePath= path.join(process.cwd(),"/supreme") ;
-        fs.readdir(supremePath,function(error,files){
-            var loop=0;
-            for(var i=0;i<files.length;i++){
-                var file=files[i];
-                var filePath=path.join(supremePath,file);
-                var stat = fs.statSync(filePath)
-                if (error) {
-                    console.error(error);
-                    throw error;
-                }
-                if(stat.isDirectory()){
-                    if(filePath.indexOf(".git")==-1){
-                        copydir(filePath,path.join(process.cwd(),file),function(err){
-                            ++loop;
-                            if(loop==files.length-1){
-                                deleteFolder(supremePath);
-                            }
-                            if(error){
-                                console.error(error);
-                                throw error;
-                            }
-                        })
-                    }
-                }else{
-                    //console.log('file:'+filePath);
+exec("git clone https://git.nevint.com/arthur/supreme.git",function(error){
+  if(!error){
+    var supremePath= path.join(process.cwd(),"/supreme") ;
+    fs.readdir(supremePath,function(error,files){
+      var loop=0;
+      for(var i=0;i<files.length;i++){
+        var file=files[i];
+        var filePath=path.join(supremePath,file);
+        var stat = fs.statSync(filePath)
+        if (error) {
+          console.error(error);
+          throw error;
+        }
+        if(stat.isDirectory()){
+          if(filePath.indexOf(".git")==-1){
+            copydir(filePath,path.join(process.cwd(),file),function(err){
+              ++loop;
+              if(loop==files.length-1){
+                console.log(chalk.green('supreme install has success...'));
+                deleteFolder(supremePath);
+              }
+              if(error){
+                console.error(error);
+                throw error;
+              }
+            })
+          }
+        }else{
+          //console.log('file:'+filePath);
 
-                    var readStream = fs.createReadStream(filePath);
-                    var writeStream = fs.createWriteStream(path.join(process.cwd(),file));
-                    var result = readStream.pipe(writeStream);
-                    ++loop;
-                    if(loop==files.length-1){
-                        deleteFolder(supremePath);
-                    }
+          var readStream = fs.createReadStream(filePath);
+          var writeStream = fs.createWriteStream(path.join(process.cwd(),file));
+          var result = readStream.pipe(writeStream);
+          ++loop;
+          if(loop==files.length-1){
+            console.log(chalk.green('supreme install has success...'));
+            deleteFolder(supremePath);
+          }
 
-                }
+        }
 
-            }
-        });
-        //
-    }
+      }
+    });
+    //
+  }
 })
 
 
 function deleteFolder(path) {
-    var files = [];
-    if( fs.existsSync(path) ) {
-        files = fs.readdirSync(path);
-        files.forEach(function(file,index){
-            var curPath = path + "/" + file;
-            if(fs.statSync(curPath).isDirectory()) { // recurse
-                deleteFolder(curPath);
-            } else { // delete file
-                fs.unlinkSync(curPath);
-            }
-        });
-        fs.rmdirSync(path);
-    }
+  var files = [];
+  if( fs.existsSync(path) ) {
+    files = fs.readdirSync(path);
+    files.forEach(function(file,index){
+      var curPath = path + "/" + file;
+      if(fs.statSync(curPath).isDirectory()) { // recurse
+        deleteFolder(curPath);
+      } else { // delete file
+        fs.unlinkSync(curPath);
+      }
+    });
+    fs.rmdirSync(path);
+  }
 }
